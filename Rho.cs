9184bc06b081dc63d275ad3ae3c73e8705cb4c19
jorgepauloS <n_i_ho@hotmail.com>
2017-04-06 13:22:51 -0300

    Add files via upload

---
+++ Rho.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cript162.Cifras
+{
+    class Rho
+    {
+        public static string GetFactorCount(int numberToCheck)
+        {
+            string output = "";
+            int factorCount = 0;
+            int sqrt = (int)Math.Ceiling(Math.Sqrt(numberToCheck));
+            output += numberToCheck.ToString() + " \n ";
+            // Começa de 1 se nós quisermos também feito para funcionar quando numberToCheck seja 0 ou 1.
+            for (int i = numberToCheck-1; i >= 1; i--)
+            {
+                if (numberToCheck % i == 0)
+                {
+                    factorCount += 2; //  Encontrou um par de fatores
+                    output += i.ToString()+" \n ";
+                }
+            }
+            // Checa se o número é um quadrado perfeito.
+            if (sqrt * sqrt == numberToCheck)
+            {
+                factorCount++;
+            }
+            output += factorCount.ToString()+" fatores.";
+            return output;
+        }
+    }
+}
